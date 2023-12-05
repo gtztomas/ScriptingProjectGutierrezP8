279b1f56f1480b8f2edca91eed0f622338775018
Tomas Gutierrez <723369@student.dusd.net>
2023-12-05 15:31:09 -0800

    added script
    added awake and start script

--- ScriptingProjectP8Gutierrez/Assets/Scripts/DoWhileLoops.cs
+++ ScriptingProjectP8Gutierrez/Assets/Scripts/DoWhileLoops.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class DoWhileLoops : MonoBehaviour
 {
     void Start()
     {
