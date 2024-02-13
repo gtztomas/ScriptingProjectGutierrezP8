1ed3ad3bdb1c8c3f9fc26332d1359bd3718a0667
Tomas Gutierrez <723369@student.dusd.net>
2024-02-13 15:19:35 -0800

    3 Destroy Scripts

---
+++ ScriptingProjectP8Gutierrez/Assets/Scripts/DestroyBasic.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBasic : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
+    
--- ScriptingProjectP8Gutierrez/Assets/Scripts/LinearInterpolation.cs
+++ ScriptingProjectP8Gutierrez/Assets/Scripts/LinearInterpolation.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LinearInterpolation : MonoBehaviour
-{
+{/*
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +13,8 @@ public class LinearInterpolation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+     //
+   
     }
+  */
 }
